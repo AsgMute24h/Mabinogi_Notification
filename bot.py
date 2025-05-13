0a432c216f1376439ad951558721c44a8d58732c
AsgMute24h <93786304+AsgMute24h@users.noreply.github.com>
2025-05-13 13:55:51 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,43 @@
+import os
+import discord
+from discord.ext import tasks, commands
+from dotenv import load_dotenv
+from datetime import datetime
+import pytz
+import keep_alive
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+korea = pytz.timezone('Asia/Seoul')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is online')
+    notify_time.start()
+
+@tasks.loop(minutes=1)
+async def notify_time():
+    now = datetime.now(korea)
+    hour = now.hour
+    if now.minute == 0:
+        channel = bot.get_channel(CHANNEL_ID)
+        if not channel:
+            return
+
+        group_a = {3, 6, 9, 12, 15, 18, 21, 0}
+        group_b = {12, 18, 20, 22}
+
+        if hour in group_a:
+            await channel.send("@everyone 불길한 소환의 결계가 나타난 것 같다.".format(hour))
+
+        if hour in group_b:
+            await channel.send("@everyone 필드 보스가 출현했습니다.".format(hour))
+
+keep_alive.keep_alive()
+bot.run(TOKEN)
